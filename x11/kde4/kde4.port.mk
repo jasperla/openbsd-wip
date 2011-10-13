5ca406391c9cba953a248d439b019a365a0d5d0c
Vadim Zhukov <persgray@gmail.com>
2011-10-13 23:46:15 +0400

    Fix automoc4 compilation. Noticed by Kirill Bychkov, thanks!

--- x11/kde4/kde4.port.mk
+++ x11/kde4/kde4.port.mk
@@ -47,7 +47,12 @@ MODKDE4_USE ?=		runtime
 MODKDE4_USE ?=		libs
 .endif
 
+# Small hack, until automoc4 will be gone
+.if !${PKGNAME:Mautomoc4}
 MODKDE4_BUILD_DEPENDS =	x11/kde4/automoc
+.else
+MODKDE4_BUILD_DEPENDS =
+.endif
 MODKDE4_LIB_DEPENDS =
 MODKDE4_RUN_DEPENDS =
 
