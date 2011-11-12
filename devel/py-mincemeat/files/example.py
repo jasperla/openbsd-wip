35eb46328c22bd0ad9258eedb607c3c2ce5c2443
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2011-11-12 11:10:01 +0100

    two wip ports, emacs-haskell is done.

---
+++ devel/py-mincemeat/files/example.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import mincemeat
+
+data = ["Humpty Dumpty sat on a wall",
+        "Humpty Dumpty had a great fall",
+        "All the King's horses and all the King's men",
+        "Couldn't put Humpty together again",
+        ]
+# The data source can be any dictionary-like object
+datasource = dict(enumerate(data))
+
+def mapfn(k, v):
+    for w in v.split():
+        yield w, 1
+
+def reducefn(k, vs):
+    result = sum(vs)
+    return result
+
+s = mincemeat.Server()
+s.datasource = datasource
+s.mapfn = mapfn
+s.reducefn = reducefn
+
+results = s.run_server(password="changeme")
+print results
