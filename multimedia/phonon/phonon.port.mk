917231b3878a2939a9ef54b1bdbf83ad9d4c067c
Vadim Zhukov <persgray@gmail.com>
2012-02-06 02:26:14 +0400

    Somewhat big Phonon update:  - Phonon library bumped to 4.6.0  - Backends forced to use Phonon configuration (thus patches to CMakeLists.txt) Goes in pair with recent QZeitgeist update.

--- multimedia/phonon/phonon.port.mk
+++ multimedia/phonon/phonon.port.mk
@@ -1,9 +1,9 @@
 
 # Remember to sync this with LIBphonon_VERSION
-MODPHONON_LIB_VERSION =	6
+MODPHONON_LIB_VERSION =	7
 
 MODPHONON_WANTLIB =	phonon=${MODPHONON_LIB_VERSION}
-MODPHONON_LIB_DEPENDS =	phonon->=4.5.1:multimedia/phonon
+MODPHONON_LIB_DEPENDS =	phonon->=4.6.0:multimedia/phonon
 MODPHONON_RUN_DEPENDS =	phonon-gstreamer-*|phonon-vlc-*:multimedia/phonon-backend/gstreamer
 
 MODULES +=	x11/qt4
