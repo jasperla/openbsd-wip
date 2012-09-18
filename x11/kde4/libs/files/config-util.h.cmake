436f8629def310243658614b528671934eda2a1d
Vadim Zhukov <persgray@gmail.com>
2012-09-18 22:59:31 +0400

    kdelibs KSDC reworked (again). This is not what will be in 4.9.x, only spinlocks went there. CMake stuff is probably for 4.10, and upstream doesn't look like to be interested in file-based locking.

--- x11/kde4/libs/files/config-util.h.cmake
+++ x11/kde4/libs/files/config-util.h.cmake
@@ -5,7 +5,15 @@
 /********** structs used by kdecore/util *********/
 
 /* functions */
-#cmakedefine   HAVE_LOCKF
 #cmakedefine   HAVE_FLOCK
+#cmakedefine   HAVE_GCC_SYNC
+#cmakedefine   HAVE_LOCKF
+#cmakedefine   HAVE_MSYNC
+#cmakedefine   HAVE_MONOTONIC_CLOCK
+#cmakedefine   HAVE_POSIX_FALLOCATE
+#cmakedefine   HAVE_PTHREAD_TIMEOUTS
+#cmakedefine   HAVE_SHARED_PTHREAD_MUTEXES
+#cmakedefine   HAVE_SHARED_SEMAPHORES
+#cmakedefine   HAVE_SHARED_SEMAPHORES_TIMEOUTS
 
 #endif /* CONFIG_UTIL_H */
