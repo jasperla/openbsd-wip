e604449e9c59bae571e930cc43ddcaf9d3400cb6
Todd T. Fries <todd@fries.net>
2012-11-28 17:09:54 -0600

    AFS_64BIT_ENV != __LP64__ doh!

--- net/openafs/files/param.obsd.h
+++ net/openafs/files/param.obsd.h
@@ -17,9 +17,7 @@
 #define AFS_X86_XBSD_ENV	1
 
 #define AFS_NAMEI_ENV		1	/* User space interface to file system */
-#if defined(__LP64__)
 #define AFS_64BIT_ENV		1
-#endif
 #define AFS_64BIT_CLIENT	1
 #define AFS_64BIT_IOPS_ENV	1	/* Needed for NAMEI */
 #define AFS_OBSD_ENV		1
