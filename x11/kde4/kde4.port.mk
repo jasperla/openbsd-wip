ca13e55bebc6a422217d8001fe8701bbfcc8f5b1
Vadim Zhukov <persgray@gmail.com>
2013-01-17 17:12:39 +0400

    Merge KDE 4.9.5 from testing directory.

--- x11/kde4/kde4.port.mk
+++ x11/kde4/kde4.port.mk
@@ -1,7 +1,7 @@
 # $OpenBSD$
 
 # The version of KDE SC in x11/kde4
-_MODKDE4_STABLE =	4.9.3
+_MODKDE4_STABLE =	4.9.5
 
 # Should be changed only by Makefile.inc in test directories
 MODKDE4_VERSION ?=	${_MODKDE4_STABLE}
