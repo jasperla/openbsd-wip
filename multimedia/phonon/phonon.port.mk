33b7ad0abdf660af90fd8d3519ab1c80dcaee675
Vadim Zhukov <persgray@gmail.com>
2013-03-03 23:57:57 +0400

    More correct WANTLIB for Phonon. Really requires bumping all dependant packages, but we'll move to 4.10.1 soon anyway, and this is WIP.

--- multimedia/phonon/phonon.port.mk
+++ multimedia/phonon/phonon.port.mk
@@ -1,9 +1,9 @@
 # $OpenBSD$
-MODPHONON_WANTLIB =	phonon_s
+MODPHONON_WANTLIB =	${MODKDE4_LIB_DIR}/phonon_s
 MODPHONON_LIB_DEPENDS =	phonon->=4.6.0p2:multimedia/phonon
 MODPHONON_RUN_DEPENDS =	phonon-gstreamer-*|phonon-vlc-*:multimedia/phonon-backend/gstreamer
 
-MODULES +=	x11/qt4
+MODULES +=	x11/kde4
 WANTLIB +=	${MODPHONON_WANTLIB}
 LIB_DEPENDS +=	${MODPHONON_LIB_DEPENDS}
 
