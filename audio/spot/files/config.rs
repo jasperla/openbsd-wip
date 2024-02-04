3ea3ac6e8ba5fedd0c26bff1bdb8ef51bf18ea64
Klemens Nanni <kn@openbsd.org>
2024-02-04 20:29:46 +0100

    spot: gc unused files

--- audio/spot/files/config.rs
+++
@@ -1,4 +0,0 @@
-pub static PKGDATADIR: &str = "${LOCALBASE}/share/${PKGSTEM}";
-pub static VERSION: &str = "${V}";
-pub static LOCALEDIR: &str = "${LOCALBASE}/share/locale";
-pub static APPID: &str = "dev.alextren.Spot";
