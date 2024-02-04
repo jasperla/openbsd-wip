5ff73c9a53d8906cf991c0c625d55538ec6c12ae
Klemens Nanni <kn@openbsd.org>
2024-02-04 16:49:30 +0100

    spot: second build fix from tb, thanks!

---
+++ audio/spot/files/config.rs
@@ -0,0 +1,4 @@
+pub static PKGDATADIR: &str = "${LOCALBASE}/share/${PKGSTEM}";
+pub static VERSION: &str = "${V}";
+pub static LOCALEDIR: &str = "${LOCALBASE}/share/locale";
+pub static APPID: &str = "dev.alextren.Spot";
