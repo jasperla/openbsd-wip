5948130626d32851498bc3b6fc90f0f53c747590
c0dev0id <sh+github@codevoid.de>
2026-03-15 19:58:01 +0100

    net/gurk-rs: update to 0.9.0, and vendor all 23 git crates
    If someone has a better idea - go ahead.

--- net/gurk-rs/files/create_vendor_tarball.sh
+++ net/gurk-rs/files/create_vendor_tarball.sh
@@ -7,34 +7,43 @@ echo "Vendoring gurk-rs $1 in /tmp ... please wait."
 rm -rf /tmp/gurk-rs*
 
 # DL
-ftp -C https://github.com/boxdot/gurk-rs/archive/refs/tags/v$v.tar.gz
+ftp -o v$v.tar.gz https://github.com/boxdot/gurk-rs/archive/refs/tags/v$v.tar.gz
 tar xzvf v$v.tar.gz
 cd gurk-rs-$v
 
-# vendor
+# vendor (stdout shows the required cargo config)
 cargo vendor | tee /tmp/gurk-rs-$v.config
 
-# pack
+# pack only the git-sourced crates (registry crates come from crates.inc)
 tar czvf gurk-rs-$v-vendorfiles.tar.gz \
     vendor/curve25519-dalek \
     vendor/curve25519-dalek-derive \
+    vendor/libsignal-account-keys \
+    vendor/libsignal-core \
     vendor/libsignal-protocol \
     vendor/libsignal-service \
-    vendor/libsignal-core \
-    vendor/libsignal-service-hyper \
-    vendor/notify-rust \
+    vendor/libsqlite3-sys \
     vendor/poksho \
     vendor/presage \
-    vendor/presage-store-sled \
-    vendor/presage-store-cipher \
-    vendor/qr2term \
+    vendor/presage-store-sqlite \
     vendor/signal-crypto \
-    vendor/zkgroup \
-    vendor/zkcredential
+    vendor/spqr \
+    vendor/sqlcipher-crypto-provider \
+    vendor/sqlx \
+    vendor/sqlx-core \
+    vendor/sqlx-macros \
+    vendor/sqlx-macros-core \
+    vendor/sqlx-mysql \
+    vendor/sqlx-postgres \
+    vendor/sqlx-sqlite \
+    vendor/usernames \
+    vendor/zkcredential \
+    vendor/zkgroup
 
 mv gurk-rs-$v-vendorfiles.tar.gz /tmp
 
-# show config
+# show cargo source config (for updating files/config)
+echo "=== cargo source config ==="
 cat /tmp/gurk-rs-$v.config
 
-echo "rsync -P /tmp/gurk-rs-$v-vendorfiles.tar.gz  sdk@codevoid.de:/home/www/htdocs/http/"
+echo "rsync -P /tmp/gurk-rs-$v-vendorfiles.tar.gz sdk@codevoid.de:/home/www/htdocs/http/"
